1db87f27083a490c185a950b2e3ce67594db05f8
esciafardini <edward.ciafardini@aclaimant.com>
2023-04-10 14:16:53 -0400

    Generate buttons with cljs instead of html

--- src/main/calculator/core.cljs
+++ src/main/calculator/core.cljs
@@ -1,4 +1,25 @@
-(ns calculator.core)
+(ns calculator.core
+  (:require [goog.dom :as dom]
+            #_[goog.events :as events]))
+
+(def total (atom 0))
+(def temp-input-zone (atom nil))
+(def operator (atom nil))
+
+(defn event-target-value [e]
+  (.-value (.-target e)))
+
+(def button-types ["7" "8" "9" "+" "4" "5" "6" "-" "1" "2" "3" "x" "C" "0" "=" "/"])
+
+(defn could-it-be []
+  (let [keys-container (dom/getElement "keys")]
+    (doall (for [button-type button-types
+                 :let [new-div (dom/createElement "button")]]
+             (do
+               (dom/classlist.add new-div "glow-on-hover")
+               (set! (.. new-div -id) button-type)
+               (set! (.. new-div -textContent) button-type)
+               (dom/appendChild keys-container new-div))))))
 
 (defn init []
-  (.log js/console "HEY I WANT IT"))
+  (could-it-be))
